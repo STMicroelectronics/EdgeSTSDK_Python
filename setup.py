8751a97bbaec87f0b525e6c8871a57c8b7a472dd
Davidroid <davide.aliprandi@gmail.com>
2019-05-29 19:33:30 +0200

    Update setup.py.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="edge_st_sdk",
-    version="1.1.0",
+    version="1.2.0",
     author="Davide Aliprandi",
     author_email="davide.aliprandi@gmail.com",
     description="IoT edge computing abstraction library package.",
